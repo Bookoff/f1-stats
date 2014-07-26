aa451c6069c7ed5404ff28298fc45d7e400f41f5
John Cheesman <info@johncheesman.org.uk>
2014-07-26 19:20:22 +0100

    Finish titles by nation

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -2,13 +2,35 @@
 
 var f1StatsControllers = angular.module('f1StatsControllers', []);
 
-f1StatsControllers.controller('driversAll', ['$scope', 'Series', function($scope, Series) {
-    $scope.drivers = null;
-    Series.get({
-        query: 'drivers.json',
-        queryString: 'limit=1000'
-    })
-    .$promise.then(function(data) {
-        $scope.drivers = data.MRData.DriverTable.Drivers;
-    });
+f1StatsControllers.controller('getDriversChampions', ['$scope', 'driversChampions', 'sortArray', 'buildArray', function($scope, driversChampions, sortArray, buildArray) {
+    $scope.driversChampions = null;
+    $scope.drivers = [];
+    driversChampions.get()
+        .$promise.then(function(data) {
+            $scope.driversChampions = data.MRData.StandingsTable.StandingsLists;
+
+            for (var i=0; i<$scope.driversChampions.length; i++) {
+                $scope.drivers[i] = $scope.driversChampions[i].DriverStandings[0].Driver.nationality;
+            }
+
+            var talliedDrivers = sortArray.tally($scope.drivers);
+            $scope.driversChampions = buildArray.nation(talliedDrivers);
+
+        });
+}]);
+
+f1StatsControllers.controller('getConstructorsChampions', ['$scope', 'constructorsChampions', 'sortArray', 'buildArray', function($scope, constructorsChampions, sortArray, buildArray) {
+    $scope.constructorsChampions = null;
+    $scope.constructors = [];
+    constructorsChampions.get()
+        .$promise.then(function(data) {
+            $scope.constructorsChampions = data.MRData.StandingsTable.StandingsLists;
+
+            for (var i=0; i<$scope.constructorsChampions.length; i++) {
+                $scope.constructors[i] = $scope.constructorsChampions[i].ConstructorStandings[0].Constructor.nationality;
+            }
+
+            var talliedConstructors = sortArray.tally($scope.constructors);
+            $scope.constructorsChampions = buildArray.nation(talliedConstructors);
+        });
 }]);
--- web/js/services.js
+++ web/js/services.js
@@ -2,71 +2,70 @@
 
 var f1StatsServices = angular.module('f1StatsServices', ['ngResource']);
 
-f1StatsServices.factory('Series', [
+f1StatsServices.factory('driversChampions', [
     '$resource',
     function($resource){
-        return $resource('http://ergast.com/api/:series/:query?:queryString',
-        {
-            query: '@query',
-            queryString: '@queryString'
-        },
+        return $resource('http://ergast.com/api/:series/:query/:queryValue?:queryString',
+        {},
         {
             get: {
                 method: 'GET',
                 params: {
                     series: 'f1',
-                    query: '',
-                    queryString: ''
+                    query: 'driverStandings',
+                    queryValue: '1.json',
+                    queryString: 'limit=100'
                 }
             }
         });
     }
 ]);
 
-f1StatsServices.factory('Season', [
+f1StatsServices.factory('constructorsChampions', [
     '$resource',
     function($resource){
-        return $resource('http://ergast.com/api/:series/:season/:query?queryString',
-        {
-            season: '@season',
-            query: '@query',
-            queryString: '@queryString'
-        },
+        return $resource('http://ergast.com/api/:series/:query/:queryValue?:queryString',
+        {},
         {
             get: {
                 method: 'GET',
                 params: {
                     series: 'f1',
-                    season: '',
-                    query: '',
-                    queryString: ''
+                    query: 'constructorStandings',
+                    queryValue: '1.json',
+                    queryString: 'limit=100'
                 }
             }
         });
     }
 ]);
 
-f1StatsServices.factory('Round', [
-    '$resource',
-    function($resource){
-        return $resource('http://ergast.com/api/:series/:season/:round/:query?queryString',
-        {
-            season: '@season',
-            round: '@round',
-            query: '@query',
-            queryString: '@queryString'
-        },
-        {
-            get: {
-                method: 'GET',
-                params: {
-                    series: 'f1',
-                    season: '',
-                    round: '',
-                    query: '',
-                    queryString: ''
-                }
+f1StatsServices.service('sortArray', function() {
+    this.tally = function(array) {
+        for (var resultObject = {}, item, i = array.length; i--;) {
+            if (!((item = array[i]) in resultObject)) {
+                resultObject[item] = 0; // Initialize a new counter
             }
-        });
+            resultObject[item]++;
+        }
+
+        return resultObject;
     }
-]);
+});
+
+f1StatsServices.service('buildArray', function() {
+    this.nation = function(object) {
+        var i = 0,
+            resultArray = [];
+
+        for (var key in object) {
+            resultArray[i] = {
+                nationality: key,
+                titles: object[key]
+            };
+            i++;
+        }
+
+        return resultArray;
+    }
+});
