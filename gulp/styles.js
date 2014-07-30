31583b33777133815fc8ec234b4fbeceee183c2f
John Cheesman <info@johncheesman.org.uk>
2014-07-30 06:16:37 +0100

    Add autoprefixer to gulp styles task

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,11 +1,13 @@
-var gulp      = require('gulp');
-var minifycss = require('gulp-minify-css');
-var sass      = require('gulp-sass');
-var rename    = require('gulp-rename');
+var gulp         = require('gulp');
+var minifycss    = require('gulp-minify-css');
+var sass         = require('gulp-sass');
+var rename       = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
     gulp.src('assets/scss/**/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer(['last 2 versions', 'ie 9']))
         .pipe(rename('style.css'))
         .pipe(minifycss())
         .pipe(gulp.dest('web/css/'));
